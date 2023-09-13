368a02e2de677b327f5e48966c7d11042d2f2b2b
Niels <niels@MRpanda.rev.sfr.net>
2023-09-13 17:02:33 +0200

    ajout de plusieurs élément mais ça casse les couilles le web c'est d'la merde

--- inte_M1/src/App.svelte
+++ inte_M1/src/App.svelte
@@ -2,79 +2,126 @@
 </script>
 
 <main>
-  <!-- navbar en haut de page -->
-
-  <div class="navbar bg-base-300 ">
-    <div class="flex-1">
-      <a href="https://www.uvsq.fr/" class="btn btn-ghost normal-case text-xl">UVSQ</a>
+  <!-- grill de la page-->
+  <div class="grid grid-col-1 gap-4">
+    <!-- navbar en haut de page -->
+    <div class="navbar bg-base-300">
+      <div class="flex-1">
+        <a href="https://www.uvsq.fr/" class="btn btn-ghost normal-case text-xl"
+          >UVSQ</a
+        >
+      </div>
+      <div class="flex-none">
+        <ul class="menu menu-horizontal px-1">
+          <li>
+            <a href="https://instagram.com/gs.isn?igshid=NjIwNzIyMDk2Mg=="
+              >instagram</a
+            >
+          </li>
+          <li><a href="https://discord.gg/hJ5nRewRpX">Discord</a></li>
+        </ul>
+      </div>
     </div>
-    <div class="flex-none">
-      <ul class="menu menu-horizontal px-1">
-        <li><a href="https://instagram.com/gs.isn?igshid=NjIwNzIyMDk2Mg==" >instagram</a></li>
-        <li><a href="https://discord.gg/hJ5nRewRpX">Discord</a></li>
-      </ul>
+
+    <div>
+      <p>
+        Bienvenue a tous et a toutes pour cette journée d'intégration. Le but de
+        cette journée est que vous appreniez a vous connaitres et que vous
+        passiez un bon moment. A la clef de cette évènement certaines équipes
+        auront le droit a des prix. De plus une collation est offert ainsi que
+        des pulls en fin d'évènement.
+      </p>
     </div>
-  </div>
 
-  <!-- étape 1 : fermat -> buffon -->
-  <div>
-    <!-- plan de l'étape-->
-    <div class="artboard phone-4">
-      <img src="src/planfermat--buffon.jpg" alt="">
+    <!-- étape 1 : fermat -> buffon -->
+    <div class="grid grid-cols-2 gap-1">
+      <!-- colonne 1-->
+      <div class="artboard phone-4">
+        <!-- plan de l'étape-->
+        <img src="src/planfermat--buffon.jpg" alt="" />
+      </div>
+      <!-- colonne 2-->
+      <div>
+        <p>
+          rendez vous au batiment buffon. une fois au batiment monter au premier
+          étage et repondez a la question
+        </p>
+
+        <p>quelle association est sur le campus parmis les réponse ?</p>
+
+        <!-- réponse possible-->
+        <button class="btn">InOO1</button>
+        <button class="btn">Malibu</button>
+        <button class="btn">PCFSchool</button>
+      </div>
     </div>
-    <p>
-      rendez vous au batiment buffon. une fois au batiment monter au premier étage et repondez a la question
-    </p>
-  </div>
-  
 
-  <!-- étape 2 : buffon -> germain/crous -->
-  <div>
-    <!-- plan de l'étape-->
-    <div class="artboard phone-4">
-      <img src="src/planbuffon--germain.jpg" alt="">
+    <!-- étape 2 : buffon -> germain/crous -->
+    <div class="grid grid-cols-2 gap-1">
+      <!-- colonne 1-->
+      <div class="artboard phone-4">
+        <!-- plan de l'étape-->
+        <img src="src/planbuffon--germain.jpg" alt="" />
+      </div>
+      <!-- colonne 2-->
+      <div>
+        <p>
+          sortez du batiment buffon. Montez la pente et rendez vous au batiment
+          germain devant la porte du crous/restaurant universitaire. Puis
+          répondez a la question:
+        </p>
+        <p>qu'es ce qu'une graduate school ?</p>
+      </div>
     </div>
-    <p>
-      sortez du batiment buffon. Montez la pente et rendez vous au batiment germain devant la porte du crous/restaurant universitaire.
-    </p>
-  </div>
 
-  <!-- étape 3 : germain/crous -> Descartes -->
-  <div>
-    <!-- plan de l'étape-->
-    <div class="artboard phone-4">
-      <img src="src/plangermain--descartes.jpg" alt="">
+    <!-- étape 3 : germain/crous -> Descartes -->
+    <div class="grid grid-cols-2 gap-1">
+      <!-- colonne 1-->
+      <div class="artboard phone-4">
+        <!-- plan de l'étape-->
+        <img src="src/plangermain--descartes.jpg" alt="" />
+      </div>
+      <!-- colonne 2-->
+      <div>
+        <p>
+          Partez du batiment Germain en direction du batiment Descartes puis
+          prenez vous en photo devant descartes ou avec la vu qu'offre la
+          terrase de descartes puis envoyer la photo au compte instagramme:
+        </p>
+        <button class="btn">Instagram</button>
+      </div>
     </div>
-    <p>
-      Partez du batiment Germain en direction du batiment Descartes puis prenez vous en photo et envoyer la au compte instagramme:
-      <a href="https://instagram.com/gs.isn?igshid=NjIwNzIyMDk2Mg=="> instagram de la graduate school isn</a>
-    </p>
-  </div>
 
-  <!-- étape 4 : Descartes -> BU -->
-  <div>
-    <!-- plan de l'étape-->
-    <div class="artboard phone-4">
-      <img src="src/plandescartes--bu.jpg" alt="">
+    <!-- étape 4 : Descartes -> BU -->
+    <div class="grid grid-cols-2 gap-1">
+      <!-- colonne 1-->
+      <div class="artboard phone-4">
+        <!-- plan de l'étape-->
+        <img src="src/plandescartes-bu.jpg" alt="" />
+      </div>
+      <!-- colonne 2-->
+      <div>
+        <p>
+          Une fois la photo faite passez par la foret pour vous rendre a la
+          bibliothèque universitaire et répondez a la question:
+        </p>
+        <p>que signifie ISN ?</p>
+      </div>
     </div>
-    <p>
-      Une fois la photo faite passez par la foret pour vous rendre a la bibliothèque universitaire et répondez a la question:
 
-    </p>
-  </div>
+    <!-- étape 5 : BU -> Stades -->
+    <div>
+      <p>
+        Bravo vous avez fini la visite de la fac. Maintenant rendez vous au
+        stade a coté de la bibliothèque universitaire pour faire quels épreuves
+        mentales et sportives.
+      </p>
+    </div>
 
-  <!-- étape 5 : BU -> Stades -->
-  <div>
-    <p>
-      Bravo vous avez fini la visite de la fac. Maintenant rendez vous au stade a coté de la bibliothèque universitaire pour faire quels épreuves mentales et sportives.
-    </p>
-  </div>
-
-  <!-- étape 6 : épreuves sportives -->
-  <div >
-    <p>
-      description des épreuves
-    </p>
+    <!-- étape 6 : épreuves sportives -->
+    <div>
+      <p>description des épreuves</p>
+    </div>
   </div>
 </main>
 
