d94a6421f759681fff1b37392dd14a1033df5ba7
nielsmerceron <nielsmpro@gmail.com>
2023-09-13 18:27:06 +0200

    ajout de règle pour les sports

--- inte_M1/src/App.svelte
+++ inte_M1/src/App.svelte
@@ -2,14 +2,17 @@
   // Votre code JavaScript ici
 </script>
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <main class="container mx-auto p-4">
   <!-- Grille de la page -->
   <div class="grid grid-cols-1 gap-4">
     <!-- Barre de navigation en haut de la page -->
     <div class="navbar bg-base-300">
       <div class="flex-1">
-        <a href="https://www.uvsq.fr/" class="btn btn-ghost normal-case text-xl">
+        <a
+          href="https://www.uvsq.fr/"
+          class="btn btn-ghost normal-case text-xl"
+        >
           UVSQ
         </a>
       </div>
@@ -27,8 +30,11 @@
 
     <div>
       <p>
-        Bienvenue à tous et à toutes pour cette journée d'intégration.
-        Le but de cette journée est que vous appreniez à vous connaître et que vous passiez un bon moment. À la clé de cet événement, certaines équipes auront le droit à des prix. De plus, une collation est offerte ainsi que des pulls à la fin de l'événement.
+        Bienvenue à tous et à toutes pour cette journée d'intégration. Le but de
+        cette journée est que vous appreniez à vous connaître et que vous
+        passiez un bon moment. À la clé de cet événement, certaines équipes
+        auront le droit à des prix. De plus, une collation est offerte ainsi que
+        des pulls à la fin de l'événement.
       </p>
     </div>
 
@@ -42,16 +48,18 @@
       <!-- Colonne 2 -->
       <div>
         <p>
-          Rendez-vous au bâtiment Buffon. Une fois au bâtiment, montez au premier étage et répondez à la question :
+          Rendez-vous au bâtiment Buffon. Une fois au bâtiment, montez au
+          premier étage et répondez à la question :
         </p>
-        <p>Quelle association est sur le campus parmi les réponses suivantes ?</p>
+        <p>
+          Quelle association est sur le campus parmi les réponses suivantes ?
+        </p>
         <!-- Réponses possibles -->
         <button class="btn">InOO1</button>
         <button class="btn">Malibu</button>
         <button class="btn">PCFSchool</button>
       </div>
     </div>
-    
 
     <!-- Étape 2 : Buffon -> Germain/CROUS -->
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-1">
@@ -63,7 +71,9 @@
       <!-- Colonne 2 -->
       <div class="container mx-auto p-4">
         <p>
-          Sortez du bâtiment Buffon. Montez la pente et rendez-vous au bâtiment Germain, devant la porte du CROUS/restaurant universitaire. Ensuite, répondez à la question :
+          Sortez du bâtiment Buffon. Montez la pente et rendez-vous au bâtiment
+          Germain, devant la porte du CROUS/restaurant universitaire. Ensuite,
+          répondez à la question :
         </p>
         <p>Qu'est-ce qu'une graduate school ?</p>
       </div>
@@ -79,7 +89,9 @@
       <!-- Colonne 2 -->
       <div class="container mx-auto p-4">
         <p>
-          Partez du bâtiment Germain en direction du bâtiment Descartes puis prenez-vous en photo devant Descartes ou avec la vue qu'offre la terrasse de Descartes, puis envoyez la photo au compte Instagram :
+          Partez du bâtiment Germain en direction du bâtiment Descartes puis
+          prenez-vous en photo devant Descartes ou avec la vue qu'offre la
+          terrasse de Descartes, puis envoyez la photo au compte Instagram :
         </p>
         <button class="btn">Instagram</button>
       </div>
@@ -95,7 +107,8 @@
       <!-- Colonne 2 -->
       <div>
         <p>
-          Une fois la photo faite, passez par la forêt pour vous rendre à la bibliothèque universitaire et répondez à la question :
+          Une fois la photo faite, passez par la forêt pour vous rendre à la
+          bibliothèque universitaire et répondez à la question :
         </p>
         <p>Que signifie ISN ?</p>
       </div>
@@ -104,13 +117,79 @@
     <!-- Étape 5 : BU -> Stades -->
     <div>
       <p>
-        Bravo, vous avez fini la visite de la fac. Maintenant, rendez-vous au stade à côté de la bibliothèque universitaire pour faire quelles épreuves mentales et sportives.
+        Bravo, vous avez fini la visite de la fac. Maintenant, rendez-vous au
+        stade à côté de la bibliothèque universitaire pour faire quelles
+        épreuves mentales et sportives.
       </p>
     </div>
 
-    <!-- Étape 6 : Épreuves sportives -->
+    <!-- étape 6 : épreuves sportives -->
     <div>
-      <p>Description des épreuves</p>
+      <h1>Détails des épreuves</h1>
+      <h2>quelques règles</h2>
+      <p>
+        Chaque équipe doit se concerter et choisir 3 jeux parmis les 4 proposés
+      </p>
+      <p>chaque épreuve peut au maximum donné 10 points</p>
+      <div>
+        <div class="navbar" />
+      </div>
+      <h2>Liste d'épreuves</h2>
+      <h3>épreuve sportive: Balle au prisonnier</h3>
+      <p>
+        une équipe affronte une autre équipe tiré au hasard et ne joue qu'un
+        match
+      </p>
+      <p>l'équipe gagnante obtient entre 5 et 10 points</p>
+      <p>l'équipe perdante obtient entre 0 et 5 points</p>
+      <p>le temps pour remporter un match influera sur le score obtenu</p>
+      <div>
+        <div class="navbar" />
+      </div>
+      <h3>épreuve de cohésion : max de passes</h3>
+      <p>
+        une équipe affronte une autre équipe tiré au hasard et ne joue qu'un
+        match
+      </p>
+      <p>
+        l'objectif est d'effectuer le maximum de passe de balle en cumulés sur
+        un match
+      </p>
+      <p>il est autorisé d'intercepté la balle en vol</p>
+      <p>
+        un match est divisé en deux mi-temps de 2 minutes pour que chaque équipe
+        puisse engagée
+      </p>
+      <p>
+        le nombre de point attribué est déterminé une fois tous les matchs joués
+        en fonction du nombre de passe faites par chaque équipe
+      </p>
+      <div>
+        <div class="navbar" />
+      </div>
+      <h3>épreuve d'adresse : chamboule tout</h3>
+      <p>10 boites disposé en pyramide</p>
+      <p>
+        une équipe joue a la fois, chaque membre a deux tirs pour faire tomber
+        sa piramide
+      </p>
+      <p>le nombre de point est la moyenne des boites tombés pour l'équipe</p>
+      <div>
+        <div class="navbar" />
+      </div>
+      <h3>épreuve de communication : Time's up</h3>
+      <p>une équipe affronte un autre équipe en 2 fois 2 manche de 45 sec</p>
+      <p>
+        durant chaque manche , un membre d'équipe doit faire deviner a son
+        coéquipier un max de mot
+      </p>
+      <p>chaque joueur doit joué au moins une fois chaque rôle</p>
+      <p>
+        le nombre de points est attribué une fois tous les matchs joués en
+        fonction du nombre de mot deviné
+      </p>
+      <p>manche 1 : description du mot pour deviner</p>
+      <p>manche 2 : le joueur ne peut dire qu'un seul mot pour faire deviner</p>
     </div>
   </div>
 </main>
