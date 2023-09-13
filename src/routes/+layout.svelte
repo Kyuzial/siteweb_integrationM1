544071a613657e8e796c60a495aa44384797e936
Kyuzial <kyuzial@protonmail.com>
2023-09-13 23:41:58 +0200

    chore: cleanup and format code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
-<svelte:head>
-	<title>IN001</title>
-  <link rel="icon" href="/favicon.svg" />
-</svelte:head>
-
 <script>
   import "../app.postcss";
 </script>
+
+<svelte:head>
+  <title>IN001</title>
+  <link rel="icon" href="/favicon.svg" />
+</svelte:head>
 
 <html data-theme="dracula" lang="fr" />
 
