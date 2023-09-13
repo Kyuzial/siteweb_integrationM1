1ecf3d30c4195ac360428e399e083c3edff983ea
Niels <niels@MRpanda.rev.sfr.net>
2023-09-13 15:23:33 +0200

    squelette du site fait + lien

--- inte_M1/src/App.svelte
+++ inte_M1/src/App.svelte
@@ -1,35 +1,72 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
+  <!-- navbar en haut de page -->
+
+  <div class="navbar bg-base-300 ">
+    <div class="flex-1">
+      <a href="https://www.uvsq.fr/" class="btn btn-ghost normal-case text-xl">UVSQ</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li><a href="https://instagram.com/gs.isn?igshid=NjIwNzIyMDk2Mg==" >instagram</a></li>
+        <li><a href="https://discord.gg/hJ5nRewRpX">Discord</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <!-- étape 1 : fermat -> buffon -->
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <div class="artboard phone-4">375×812</div>
+    <p>
+      rendez vous au batiment buffon. une fois au batiment monter au premier étage et repondez a la question
+    </p>
   </div>
-  <h1>Vite + Svelte</h1>
+  
 
-  <div class="card">
-    <Counter />
+  <!-- étape 2 : buffon -> germain/crous -->
+  <div>
+    <div class="artboard phone-4">375×812</div>
+    <p>
+      sortez du batiment buffon. Montez la pente et rendez vous au batiment germain devant la porte du crous/restaurant universitaire.
+    </p>
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-  <button class="btn btn-primary">Primary button </button>
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <!-- étape 3 : germain/crous -> Descartes -->
+  <div>
+    <div class="artboard phone-4">375×812</div>
+    <p>
+      Partez du batiment Germain en direction du batiment Descartes puis prenez vous en photo et envoyer la au compte instagramme:
+      <a> ajouter une ref</a>
+    </p>
+  </div>
+
+  <!-- étape 4 : Descartes -> BU -->
+  <div>
+    <div class="artboard phone-4">375×812</div>
+    <p>
+      Une fois la photo faite passez par la foret pour vous rendre a la bibliothèque universitaire et répondez a la question:
+
+    </p>
+  </div>
+
+  <!-- étape 5 : BU -> Stades -->
+  <div>
+    <div class="artboard phone-4">375×812</div>
+    <p>
+      Bravo vous avez fini la visite de la fac. Maintenant rendez vous au stade a coté de la bibliothèque universitaire pour faire quels épreuves mentales et sportives.
+    </p>
+  </div>
+
+  <!-- étape 6 : épreuves sportives -->
+  <div >
+    <div class="artboard phone-4">375×812</div>
+  </div>
 </main>
 
-<style global lang="postcss" >
+<style global lang="postcss">
   @tailwind base;
-@tailwind components;
-@tailwind utilities;
+  @tailwind components;
+  @tailwind utilities;
 </style>
